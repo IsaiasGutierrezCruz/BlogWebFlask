6d427bdf1aaf587c78df1f26ec5bd2a235274136
IsaiasGutierrez <isaiascruz@comunidad.unam.mx>
2021-11-25 01:06:33 -0600

    applying inheritance in templates

--- hello.py
+++ hello.py
@@ -15,3 +15,11 @@ def about():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def interval_server_error(e):
+    return render_template('500.html'), 500
