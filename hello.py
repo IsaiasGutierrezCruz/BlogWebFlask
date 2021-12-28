9ab41967007aaa2bbb7ec2cf2383d1a9c0a81f92
IsaiasGutierrez <isaiascruz@comunidad.unam.mx>
2021-12-28 11:22:50 -0600

    form implemented

--- hello.py
+++ hello.py
@@ -1,24 +1,35 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+from flask_moment import Moment # manage dates 
+from datetime import datetime # get dates 
+# build web forms 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'qwerty'
 Bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    #user_agent = request.headers.get('User-Agent')
-    return render_template('index.html', current_time = datetime.utcnow())
+    #user_agent = request.headers.get('User-Agent') #example of a request
+    name = None
+    form = Form1()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time = datetime.utcnow(), form = form, name = name)
 
 @app.route('/about.html')
 def about():
     return '<h1>Im about</>'
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/user/<user>')
+def user(user):
+    return render_template('user.html', user=user)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -27,3 +38,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def interval_server_error(e):
     return render_template('500.html'), 500
+
+# ----- forms  --------------------
+class Form1(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
